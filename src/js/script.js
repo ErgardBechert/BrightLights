c1170087515e98cd1938bf9de33459f80203d88c
Эргард Бехерт <is53-behertev@radiotech.su>
2022-11-01 12:59:17 +0500

    121

---
+++ src/js/script.js
@@ -0,0 +1,70 @@
+/*Burger */
+const burger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header-body__menu');
+
+burger.onclick = function() {
+    burger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+    document.body.classList.toggle('lock');
+}
+/*Player*/
+
+const player = document.querySelector('.player');
+
+const audio = document.querySelector('.audio');
+let Seconds = 0;
+let Minute = 0;
+let TimePlayer = audio.duration
+
+
+const MaxTimePlayer = () =>{
+  while (TimePlayer >= 60) {
+    TimePlayer -= 60;
+    Minute +=1
+  }
+  Seconds = TimePlayer
+}
+const playerTime = document.querySelector('.player-time');
+let CurrentTime = audio.currentTime
+const CurrentTimePlayer = () =>{
+  while (CurrentTimePlayeer <= TimePlayer){
+
+    player.innerHTML += `
+    <div class="player-time">
+      ${Math.round(audio.currentTime*100)/100} - 0${Minute}:${Math.floor(Seconds)}
+    </div>
+    
+    `
+  }
+}
+// CurrentTimePlayer();
+MaxTimePlayer();
+
+function progressUpdate() {
+ 
+}
+
+player.innerHTML += `
+                   
+                   <div value='play' class="player-btn">
+                   <img src="../src/img/main/player-btn.svg" alt="" class="play-btn-img">
+                   </div>
+                   <input class="player-progress" type="range" min="0" max="10" step="0.01" value="0">
+                   <div class="player-time">
+                   ${Math.round(audio.currentTime*100)/100} - 0${Minute}:${Math.floor(Seconds)}
+                  </div>
+`
+
+
+const playerBtn = document.querySelector('.player-btn');
+
+playerBtn.addEventListener('click', function() {
+  const audio = document.querySelector('.audio');
+    if(audio.paused) {
+        audio.play();
+       
+      } else {
+        audio.pause();
+      }
+});
+progressUpdate()
